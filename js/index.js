30a38bd43d27fcc697b5eb8c1b7786884a670336
Sierra <sierra.ar.sager@gmail.com>
2024-12-19 20:57:52 -0500

    completed part 1

--- js/index.js
+++ js/index.js
@@ -2,3 +2,39 @@
 // that are not declared
 "use strict"
 
+/*
+
+const myName = "Sierra";
+undefined
+console.log(myName)
+VM147:1 Sierra
+undefined
+console.log(typeof myName)
+VM246:1 string
+undefined
+const one = 1;
+undefined
+console.log(one)
+VM328:1 1
+undefined
+console.log(typeof one)
+VM428:1 number
+undefined
+let isSleepy = true;
+undefined
+ console.log(isSleepy)
+VM553:1 true
+undefined
+console.log(typeof isSleepy)
+VM658:1 boolean
+undefined
+isSleepy = false;
+false
+console.log(isSleepy)
+VM771:1 false
+undefined
+console.log(typeof isSleepy)
+VM883:1 boolean
+undefined
+
+*/
