9b392bb5a42251d1bfd2233f6c76fb5e35adec0b
Sierra <sierra.ar.sager@gmail.com>
2024-12-19 21:15:11 -0500

    completed part 2

--- js/assignment.js
+++ js/assignment.js
@@ -8,23 +8,39 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "Stay hydrated";
+const currentYear = 2025;
+let willMeetResolution = "yes"
+
 
 function updateYear() {
     // write the logic
+    yearElement.innerText = currentYear;
 
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
 
 }
 
+function updatewillMeetResolution() {
+    willMeetResolutionElement.innerText = willMeetResolution;
+}
+
+
 function render() {
     // Finish writing this function
+    updateYear();
+    updateResolution();
+    updatewillMeetResolution();
+
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
+    render();
 
 })
